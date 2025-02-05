5b1d9fe6d06d505666edf269ecfecb2812115f6d
MabelRincon <mabel.rincon@outlook.com>
2025-02-05 15:42:10 +0100

    Save changes to GhostModelTest. WIP

--- src/test/java/dev/cachaguercus/proyecto4/models/GhostModelTest.java
+++ src/test/java/dev/cachaguercus/proyecto4/models/GhostModelTest.java
@@ -15,8 +15,6 @@ public class GhostModelTest {
 
         GhostModel ghost1 = new GhostModel(1, "ghost1", enumGhostType.CLASE_I, enumDangerLevel.ALTO, "vomitar verde", LocalDate.now());
 
-        /* GhostModel ghost1 = new GhostModel(ghost1.id, ghost1.name, ghost1.ghost_type, ghost1.danger_level, ghost1.special_skill, ghost1.capture_date);
- */
         ghost1.getId();
         ghost1.getName();
         ghost1.getGhost_type();
