89e55377f149b2e0ad03fb04e0c3b59528146f8b
MabelRincon <mabel.rincon@outlook.com>
2025-02-05 15:39:04 +0100

    Write GhostModel method. WIP

--- src/main/java/dev/cachaguercus/proyecto4/models/GhostModel.java
+++ src/main/java/dev/cachaguercus/proyecto4/models/GhostModel.java
@@ -12,4 +12,39 @@ public class GhostModel {
     private enumDangerLevel danger_level;
     private String special_skill;
     private LocalDate capture_date;
+
+    public GhostModel(int id, String name, enumGhostType ghost_type, enumDangerLevel danger_level, String special_skill,
+            LocalDate capture_date) {
+        this.id = id;
+        this.name = name;
+        this.ghost_type = ghost_type;
+        this.danger_level = danger_level;
+        this.special_skill = special_skill;
+        this.capture_date = capture_date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public enumGhostType getGhost_type() {
+        return ghost_type;
+    }
+
+    public enumDangerLevel getDanger_level() {
+        return danger_level;
+    }
+
+    public String getSpecial_skill() {
+        return special_skill;
+    }
+
+    public LocalDate getCapture_date() {
+        return capture_date;
+    }
+
 }
--- src/test/java/dev/cachaguercus/proyecto4/models/GhostModelTest.java
+++ src/test/java/dev/cachaguercus/proyecto4/models/GhostModelTest.java
@@ -3,20 +3,33 @@ package dev.cachaguercus.proyecto4.models;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import dev.cachaguercus.proyecto4.enums.enumDangerLevel;
+import dev.cachaguercus.proyecto4.enums.enumGhostType;
+import java.time.LocalDate;
+
 public class GhostModelTest {
 
     @Test
     @DisplayName("when the GB selects a ghost, all the expected info is provide")
     void testGetGhostModel() {
 
-        GhostModel ghost1 = new GhostModel(ghost1.id, ghost1.name, ghost1.ghost_type, ghost1.danger_level, ghost1.special_skill, ghost1.capture_date);
+        GhostModel ghost1 = new GhostModel(1, "ghost1", enumGhostType.CLASE_I, enumDangerLevel.ALTO, "vomitar verde", LocalDate.now());
+
+        /* GhostModel ghost1 = new GhostModel(ghost1.id, ghost1.name, ghost1.ghost_type, ghost1.danger_level, ghost1.special_skill, ghost1.capture_date);
+ */
+        ghost1.getId();
+        ghost1.getName();
+        ghost1.getGhost_type();
+        ghost1.getDanger_level();
+        ghost1.getSpecial_skill();
+        ghost1.getCapture_date();
 
         assertEquals(ghost1.id, ghost1.getId());
         assertEquals(ghost1.name, ghost1.getName());
-        assertEquals(ghost1.ghost_type, ghost1.getGhostType());
-        assertEquals(ghost1.danger_level, ghost1.getDangerLevel());
-        assertEquals(ghost1.special_skill, ghost1.getSpecialSkill());
-        assertEquals(ghost1.capture_date, ghost1.getCaptureDate());
+        assertEquals(ghost1.ghost_type, ghost1.getGhost_type());
+        assertEquals(ghost1.danger_level, ghost1.getDanger_level());
+        assertEquals(ghost1.special_skill, ghost1.getSpecial_skill());
+        assertEquals(ghost1.capture_date, ghost1.getCapture_date());
 
     }
 }
