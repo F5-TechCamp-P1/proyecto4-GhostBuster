b28e236a80b45b926f1bbf8aba6c861a0077fe54
MabelRincon <mabel.rincon@outlook.com>
2025-02-05 15:22:30 +0100

    testGetGhostModel failed

--- src/main/java/dev/cachaguercus/proyecto4/models/GhostModel.java
+++ src/main/java/dev/cachaguercus/proyecto4/models/GhostModel.java
@@ -1,5 +1,15 @@
 package dev.cachaguercus.proyecto4.models;
 
-public class GhostModel {
+import java.time.LocalDate;
 
+import dev.cachaguercus.proyecto4.enums.enumDangerLevel;
+import dev.cachaguercus.proyecto4.enums.enumGhostType;
+
+public class GhostModel {
+    private int id;
+    private String name;
+    private enumGhostType ghost_type;
+    private enumDangerLevel danger_level;
+    private String special_skill;
+    private LocalDate capture_date;
 }
--- src/test/java/dev/cachaguercus/proyecto4/models/GhostModelTest.java
+++ src/test/java/dev/cachaguercus/proyecto4/models/GhostModelTest.java
@@ -1,9 +1,22 @@
 package dev.cachaguercus.proyecto4.models;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GhostModelTest {
 
-   
-    
+    @Test
+    @DisplayName("when the GB selects a ghost, all the expected info is provide")
+    void testGetGhostModel() {
+
+        GhostModel ghost1 = new GhostModel(ghost1.id, ghost1.name, ghost1.ghost_type, ghost1.danger_level, ghost1.special_skill, ghost1.capture_date);
+
+        assertEquals(ghost1.id, ghost1.getId());
+        assertEquals(ghost1.name, ghost1.getName());
+        assertEquals(ghost1.ghost_type, ghost1.getGhostType());
+        assertEquals(ghost1.danger_level, ghost1.getDangerLevel());
+        assertEquals(ghost1.special_skill, ghost1.getSpecialSkill());
+        assertEquals(ghost1.capture_date, ghost1.getCaptureDate());
+
+    }
 }
