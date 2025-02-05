fde892fc7af057e64725b55397c3ad98264c3f47
Ayuik <marcosayelen@gmail.com>
2025-02-05 14:44:40 +0100

    testGetGhostType fails

--- src/main/java/dev/cachaguercus/proyecto4/enums/enumGhostType.java
+++ src/main/java/dev/cachaguercus/proyecto4/enums/enumGhostType.java
@@ -1,13 +1,10 @@
 package dev.cachaguercus.proyecto4.enums;
 
 public enum enumGhostType {
-
-    CLASE_I,
-    CLASE_II,
-    CLASE_III,
-    CLASE_IV,
-    CLASE_V,
-    CLASE_VI,
-    CLASE_VII
-
+    
+    
+    
+    
+    
+    
 }
--- src/test/java/dev/cachaguercus/proyecto4/models/GhostModelTest.java
+++ src/test/java/dev/cachaguercus/proyecto4/models/GhostModelTest.java
@@ -1,4 +1,13 @@
 package dev.cachaguercus.proyecto4.models;
 
+import org.junit.jupiter.api.Test;
+
 public class GhostModelTest {
+
+    @Test
+    @DisplayName("")
+    void testName() {
+        
+    }
+    
 }
