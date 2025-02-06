4d69e003ec9cea93f5922ed500b16d2656ea680c
MabelRincon <mabel.rincon@outlook.com>
2025-02-06 14:58:32 +0100

    BeforeEach and testGetGhostBusterModel fail

--- src/test/java/dev/cachaguercus/proyecto4/models/GhostBusterModelTest.java
+++ src/test/java/dev/cachaguercus/proyecto4/models/GhostBusterModelTest.java
@@ -1,4 +1,32 @@
 package dev.cachaguercus.proyecto4.models;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class GhostBusterModelTest {
+
+    private GhostBusterModel ghostBusterModel;
+
+    @BeforeEach
+    void setUp() {
+        GhostBusterModel ghostBusterModel = new GhostBusterModel();
+    }
+
+    @Test
+    @DisplayName("Should get GhostBuster name and Ghost trap")
+    void testGetGhostbusterModel() {
+
+        GhostBusterModel cachaguercu = new GhostBusterModel("cachaguercu", List<GhostModel>);
+        String name = cachaguercu.getName();
+        List<GhostModel> ghostList = cachaguercu.getList();
+
+        assertEquals("cachaguercu", name);
+        assertEquals(List<GhostModel> ghostList);
+    }
+
 }
