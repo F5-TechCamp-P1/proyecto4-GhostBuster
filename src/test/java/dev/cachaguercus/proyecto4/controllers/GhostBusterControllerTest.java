48b9965fb14062c7448e8418c5514d6d9da121f3
MabelRincon <mabel.rincon@outlook.com>
2025-02-07 12:31:49 +0100

    Folder structure: views

--- src/test/java/dev/cachaguercus/proyecto4/controllers/GhostBusterControllerTest.java
+++ src/test/java/dev/cachaguercus/proyecto4/controllers/GhostBusterControllerTest.java
@@ -1,4 +1,21 @@
 package dev.cachaguercus.proyecto4.controllers;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class GhostBusterControllerTest {
+
+    @Test
+    @DisplayName("Should run the program")
+    void testRun() {
+
+        new GhostBusterController().run();
+
+
+
+    }
+
+
+
+
 }
