1f605d474f50a14ade6ffe4dd126eba4bce3a3d5
MabelRincon <mabel.rincon@outlook.com>
2025-02-07 09:32:24 +0100

    enumDangerLevelTest passed

--- src/main/java/dev/cachaguercus/proyecto4/enums/enumDangerLevel.java
+++ src/main/java/dev/cachaguercus/proyecto4/enums/enumDangerLevel.java
@@ -2,9 +2,19 @@ package dev.cachaguercus.proyecto4.enums;
 
 public enum enumDangerLevel {
 
-    BAJO,
-    MEDIO,
-    ALTO,
-    CRITICO
+    BAJO(1),
+    MEDIO(2),
+    ALTO(3),
+    CRITICO(4);
+
+    private final int numericLevel;
+
+    enumDangerLevel(int nivelNumerico) {
+        this.numericLevel = nivelNumerico;
+    }
+
+    public int getNumericLevel() {
+        return numericLevel;
+    }
 
 }
