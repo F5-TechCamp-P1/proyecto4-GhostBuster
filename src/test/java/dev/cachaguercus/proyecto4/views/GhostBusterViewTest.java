c2b34971e4d6fdaed69e59524f360aaff5440f42
MabelRincon <mabel.rincon@outlook.com>
2025-02-07 12:45:05 +0100

    testDisplayWelcomeMessage failed

--- src/test/java/dev/cachaguercus/proyecto4/views/GhostBusterViewTest.java
+++ src/test/java/dev/cachaguercus/proyecto4/views/GhostBusterViewTest.java
@@ -1,4 +1,24 @@
 package dev.cachaguercus.proyecto4.views;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class GhostBusterViewTest {
+
+    @Test
+    @DisplayName("Should display the Welcome message and request a name to the GhostBuster")
+    void testDisplayWelcomeMessage() {
+        GhostBusterView ghostBusterView = new GhostBusterView();
+        ghostBusterView.displayWelcomeMessage();
+
+        String expectedMessage = "Inicio del Programa\n" +
+                        "============================================\n" +
+                        "    ¡Bienvenido a la Base Ghostbusters Asturias!\n" +
+                        "    Gestiona tus fantasmas atrapados y protege la región\n" +
+                        "============================================\n" +
+                        "Introduce tu nombre: ";
+        String actualMessage = ghostBusterView.getWelcomeMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
 }
