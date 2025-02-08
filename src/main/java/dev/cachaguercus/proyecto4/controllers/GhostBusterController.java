7d416ce2743c73c30dfb8e7a77019ff95b0fa17a
Ayuik <marcosayelen@gmail.com>
2025-02-08 11:48:03 +0100

    changes implemented. no tests run. WIP.

--- src/main/java/dev/cachaguercus/proyecto4/controllers/GhostBusterController.java
+++ src/main/java/dev/cachaguercus/proyecto4/controllers/GhostBusterController.java
@@ -2,25 +2,26 @@ package dev.cachaguercus.proyecto4.controllers;
 
 import java.util.Scanner;
 
-import javax.print.attribute.standard.RequestingUserName;
-
 import dev.cachaguercus.proyecto4.views.GhostBusterView;
 
 public class GhostBusterController {
     private Scanner scanner = new Scanner(System.in);
     private GhostBusterView ghostBusterView = new GhostBusterView();
+    Scanner gbInputScanner = new Scanner(System.in);
+    String selectedoption = scanner.nextLine();
 
-    public String captureGhost(){
-        Scanner gbInputScanner = new Scanner(System.in);
-        String selectedoption = scanner.nextLine();
-        if (selectedoption == "1") {
-           System.out.println(ghostBusterView.displayCaptureGhost());
-           return "método ok";
+    public String captureGhost(String selectedoption) {
+        if (selectedoption.equals("1")) {
+            System.out.println(ghostBusterView.displayCaptureGhost());
+            return "método ok";
+        } else {
+            System.out.println("Opción no valida");
+            return "método no ok";
         }
-           else {
-               System.out.println("Opción no valida");
-               return "método no ok";
-           }
-           gbInputScanner.close();
+    }
+
+    public void closeScanners() {
+        scanner.close();
+        gbInputScanner.close();
     }
 }
--- src/test/java/dev/cachaguercus/proyecto4/controllers/GhostBusterControllerTest.java
+++ src/test/java/dev/cachaguercus/proyecto4/controllers/GhostBusterControllerTest.java
@@ -3,25 +3,20 @@ package dev.cachaguercus.proyecto4.controllers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import dev.cachaguercus.proyecto4.views.GhostBusterView;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
-import java.util.Scanner;
 
 public class GhostBusterControllerTest {
 
     @Test
     @DisplayName("When the ghostbuster select option 1 from main menu, should display a request to enter the name of the ghost")
     void testCaptureGhost(){
-        Scanner gbInputScanner = new Scanner(System.in);
-        String selectedoption = scanner.nextLine();
-        GhostBusterView ghostBusterView = new GhostBusterView();
-        String expectedMessage = ghostBusterView.displayCaptureGhost();
-        selectedoption.equals("1");
         GhostBusterController ghostBusterController = new GhostBusterController();
-        ghostBusterController.captureGhost();
-        String actualMessage = ghostBusterView.displayCaptureGhost();
-        assertEquals(expectedMessage, actualMessage);
+        String selectedoption = "1";
+        String message = ghostBusterController.captureGhost(selectedoption);
+        assertThat(message, is("método ok"));
+        ghostBusterController.closeScanners();
     }
 }
