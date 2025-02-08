762099d49da61d3862b6694471deaddd960ce68c
MabelRincon <mabel.rincon@outlook.com>
2025-02-08 11:01:33 +0100

    testCaptureGhost WIP

--- src/main/java/dev/cachaguercus/proyecto4/controllers/GhostBusterController.java
+++ src/main/java/dev/cachaguercus/proyecto4/controllers/GhostBusterController.java
@@ -1,5 +1,22 @@
 package dev.cachaguercus.proyecto4.controllers;
 
-public class GhostBusterController {
+import java.util.Scanner;
 
+import dev.cachaguercus.proyecto4.views.GhostBusterView;
+
+public class GhostBusterController {
+    private Scanner scanner = new Scanner(System.in);
+    private GhostBusterView ghostBusterView = new GhostBusterView();
+
+    public void captureGhost(){
+        Scanner gbInputScanner = new Scanner(System.in);
+        String selectedoption = scanner.nextLine();
+        if (selectedoption == "1") {
+           System.out.println(ghostBusterView.displayCaptureGhost());
+        }
+           else {
+               System.out.println("Opción no valida");
+           }
+           gbInputScanner.close();
+    }
 }
