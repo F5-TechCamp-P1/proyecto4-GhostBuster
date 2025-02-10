7338bc5d7146cd589b73d723f003292c23b28281
MabelRincon <mabel.rincon@outlook.com>
2025-02-10 08:33:54 +0100

    Refactor Tests

--- src/test/java/dev/cachaguercus/proyecto4/controllers/GhostBusterControllerTest.java
+++ src/test/java/dev/cachaguercus/proyecto4/controllers/GhostBusterControllerTest.java
@@ -26,11 +26,11 @@ import java.util.Scanner;
 
 public class GhostBusterControllerTest {
 
-    private GhostBusterView view;
+   /*  private GhostBusterView view;
     private GhostBusterModel model;
     private GhostBusterController controller;
-    private InputStream originalSystemIn;
-
+    private InputStream originalSystemIn; */
+/*
     @BeforeEach
     void setUp() {
         GhostBusterView view = Mockito.mock(GhostBusterView.class);
@@ -42,18 +42,18 @@ public class GhostBusterControllerTest {
     @AfterEach
     void restoreSystemIn() {
         System.setIn(originalSystemIn);
-    }
+    } */
 
     @Test
     @DisplayName("Should ask GhostBusterView to displayWelcomeMessage")
     void testRun() {
-        /* GhostBusterView view = Mockito.mock(GhostBusterView.class);
-        GhostBusterModel model = new GhostBusterModel(); */
+        GhostBusterView view = Mockito.mock(GhostBusterView.class);
+        GhostBusterModel model = new GhostBusterModel();
         String input = "X\n4\n";
-        //System.setIn(new ByteArrayInputStream(input.getBytes()));
+       // System.setIn(new ByteArrayInputStream(input.getBytes()));
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-        //GhostBusterController controller = new GhostBusterController(model, view);
+        GhostBusterController controller = new GhostBusterController(model, view);
 
         controller.run();
         verify(view, times(1)).displayWelcomeMessage();
@@ -62,13 +62,13 @@ public class GhostBusterControllerTest {
     @Test
     @DisplayName("Should ask GhostBusterView to displayInitialMenu")
     void testSelectOptionMainMenu() {
-       /*  GhostBusterView view = Mockito.mock(GhostBusterView.class);
-        GhostBusterModel model = new GhostBusterModel(); */
+        GhostBusterView view = Mockito.mock(GhostBusterView.class);
+        GhostBusterModel model = new GhostBusterModel();
         String input = "4\n";
         //System.setIn(new ByteArrayInputStream(input.getBytes()));
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-       // GhostBusterController controller = new GhostBusterController(model, view);
+        GhostBusterController controller = new GhostBusterController(model, view);
 
         controller.selectOptionMainMenu();
         verify(view, times(1)).displayInitialMenu();
@@ -77,19 +77,19 @@ public class GhostBusterControllerTest {
     @Test
     @DisplayName("Should ask model to set the GhostBuster name")
     void testRunAsksUserForName() {
-        /* GhostBusterView view = new GhostBusterView();
-        GhostBusterModel model = Mockito.mock(GhostBusterModel.class); */
+        GhostBusterView view = new GhostBusterView();
+        GhostBusterModel model = Mockito.mock(GhostBusterModel.class);
         String input = "Cachaguercu\n4\n";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-        /* InputStream in = new ByteArrayInputStream(input.getBytes());
+        // System.setIn(new ByteArrayInputStream(input.getBytes()));
+        InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         GhostBusterController controller = new GhostBusterController(model, view);
- */
+
         controller.run();
         verify(model, times(1)).setName("Cachaguercu");
     }
 
-/*    @Test
+   @Test
     @DisplayName("When captureGhost is called, should call model.captureGhost with the correct parameters")
     void testCaptureGhost() {
         GhostBusterModel model = Mockito.mock(GhostBusterModel.class);
@@ -123,14 +123,14 @@ public class GhostBusterControllerTest {
         when(view.displayReleaseGhost()).thenReturn("Casper");
         controller.removeGhost("Casper");
         assertThat(model.getGhostTrap(), not(hasItem(ghost)));
-    } */
+    }
 
      @Test
     @DisplayName("Should display the list of captured ghosts")
     void testListGhosts() {
-    /*     GhostBusterModel model = Mockito.mock(GhostBusterModel.class);
+        GhostBusterModel model = Mockito.mock(GhostBusterModel.class);
         GhostBusterView view = Mockito.mock(GhostBusterView.class);
-        GhostBusterController controller = new GhostBusterController(model, view); */
+        GhostBusterController controller = new GhostBusterController(model, view);
         GhostModel ghost = new GhostModel(0, "Casper", enumGhostType.CLASE_I, enumDangerLevel.BAJO,
                 "aparecerse y sonreir", LocalDate.now());
         GhostBusterModel.captureGhost(ghost);
@@ -141,8 +141,8 @@ public class GhostBusterControllerTest {
     @Test
     @DisplayName("Should display exit message")
     void testExitGame() {
-       /*  GhostBusterView view = Mockito.mock(GhostBusterView.class);
-        GhostBusterController controller = new GhostBusterController(null, view); */
+        GhostBusterView view = Mockito.mock(GhostBusterView.class);
+        GhostBusterController controller = new GhostBusterController(null, view);
         controller.exitGame();
         verify(view, times(1)).displayExitMessage();
     }
